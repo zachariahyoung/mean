d8052f377ab1df7d5837fa3b5592d7ea17c25642
Zachariah Young <zpyoung@gmail.com>
2014-03-08 06:18:09 -0600

    Updated

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifer, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifer, mvAuth, $location) {
     $scope.identity = mvIdentity;
     $scope.signin = function (username, password) {
         mvAuth.authenticate(username, password).then(function (success) {
@@ -9,4 +9,15 @@ angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, m
             }
         });
     }
-})
+
+    $scope.signout = function () {
+        mvAuth.logoutUser().then(function () {
+            $scope.username = "";
+            $scope.password = "";
+            mvNotifer.notify('You have successfully signed out!');
+            $location.path('/');
+
+        })
+    }
+
+});
